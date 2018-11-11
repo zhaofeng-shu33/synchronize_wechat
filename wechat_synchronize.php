7361ab1b641c181b45dc707a6db9ffc9fcf44a45
zhaofeng-shu33 <616545598@qq.com>
2018-11-11 11:19:15 +0800

    add php meta info

---
+++ wechat_synchronize.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package wechat_synchronize
+ * @version 1.0
+ */
+/*
+Plugin Name: wechat synchronize
+Plugin URI: https://github.com/zhaofeng-shu33/wechat_synchronize_to_wordpress
+Description: synchronize wechat articles to wordpress website
+Author: zhaofeng-shu33
+Version: 0.1
+Author URI: https://github.com/zhaofeng-shu33
+*/
+?>
