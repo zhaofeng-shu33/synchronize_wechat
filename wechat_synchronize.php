979faeacb22cfbd49679baff150855f6f3860aae
zhaofeng-shu33 <616545598@qq.com>
2019-01-01 20:07:24 +0800

    test webhook

--- wechat_synchronize.php
+++ wechat_synchronize.php
@@ -33,7 +33,7 @@ function ws_process_request(){
     // if no post data, return 
     $sync_history = isset($_REQUEST['ws_history']) ? $_REQUEST['ws_history'] == 'ws_Yes' : false;
     if($sync_history){
-        if($_REQUEST['offset']){
+        if(isset($_REQUEST['offset'])){
             $return_array = array();
             ws_get_history_url_by_offset($return_array, $_REQUEST['offset']);            
         }
