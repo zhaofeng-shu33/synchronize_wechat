8cd653bae4a0925160f138308cb6a3565672bda7
zhaofeng-shu33 <616545598@qq.com>
2019-01-01 14:24:23 +0800

    no dep on curl

--- insert_by_url.php
+++ insert_by_url.php
@@ -21,15 +21,8 @@ function check_wx_url($url){
 //! \param  $url
 //! \return  $html raw text, no error handling in this function
 function get_html($url, $timeout = 30){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $html = curl_exec($ch);
-    curl_close($ch);
-    
-    return $html;
+    $response = wp_safe_remote_get( $url, array( 'timeout' => $timeout) );    
+    return wp_remote_retrieve_response_message($response);
 }
 
 /**
