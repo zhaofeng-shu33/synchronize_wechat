583dcf7e37b8c4290d5a86ac8d82fc901b6553cf
zhaofeng-shu33 <616545598@qq.com>
2019-02-12 00:35:29 +0800

    add two func

--- clients/client.py
+++ clients/client.py
@@ -1,13 +1,35 @@
 '''
    A simple Python client to interact with wordpress backend,
-   currently implementing login(general function) and ajax post for 
+   currently implementing login(general function) and ajax post.
 '''
-URL_ROOT = ''
-URL_LOGIN = ''
-SYNC_WECHAT_URL = URL_ROOT + '/wp-admin/admin-ajax.php'
+URL_ROOT = 'http://localhost'
+import yaml
 import requests
-def login(username, userpass):
+with open('config.yaml') as f:
+    json = yaml.load(f.read())
+    if(json.get('root')):
+        URL_ROOT = json['root']
+    USER_NAME = json['username']
+    USER_PASS = json['userpass']
+    
+URL_LOGIN = URL_ROOT + '/wp-login.php'
+SYNC_WECHAT_URL = URL_ROOT + '/wp-admin/admin-ajax.php'
+    
+def login():
     s = requests.Session()
-    return s
-def sync_wechat_ajax():
-    return
+    payload = {'log':USER_NAME, 'pwd':USER_PASS}
+    # corresponding to function `wp_signon` defined in `wp-includes/user.php`
+    r = s.post(URL_LOGIN, data=payload) # post form data
+    if(r.status_code == 302):
+        return s
+    else:
+        # response is html form, to be done, track the error message?
+        return False
+def sync_wechat_ajax(session_object, dic):
+    # tranditional form post
+    r = session_object.post(SYNC_WECHAT_URL, data=dic)
+    if(r.status_code == 200):
+        return r.text
+    else:
+        return False
+
