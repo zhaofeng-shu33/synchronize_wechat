fa0c4cc0e51ad765dc48d22c13762db3fbce1586
zhaofeng-shu33 <616545598@qq.com>
2019-10-14 00:42:40 +0800

    add right filename

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 svn co -q "http://plugins.svn.wordpress.org/synchronize-wechat" /tmp/svn
-rsync -rc --exclude-from="./distignore" ./ /tmp/svn/trunk --delete
+rsync -rc --exclude-from="./.distignore" ./ /tmp/svn/trunk --delete
 cd /tmp/svn
 svn add .
 svn ci --username zhaofeng-shu33 --password $PASSWORD -m "$(git log -1 --pretty=%B)"
