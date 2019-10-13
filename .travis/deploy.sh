7a4a61e0f58236531f302d6fb37dfb523d64967d
zhaofeng-shu33 <616545598@qq.com>
2019-10-14 00:54:56 +0800

    use right username

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@ svn co -q "http://plugins.svn.wordpress.org/synchronize-wechat" /tmp/svn
 rsync -rc --exclude-from="./.distignore" ./ /tmp/svn/trunk --delete
 cd /tmp/svn
 svn add . --force
-svn ci --no-auth-cache --non-interactive --username zhaofeng-shu33 --password $PASSWORD -m "$(git log -1 --pretty=%B)"
+svn ci --no-auth-cache --non-interactive --username zhaofeng_shu33 --password $PASSWORD -m "$(git log -1 --pretty=%B)"
