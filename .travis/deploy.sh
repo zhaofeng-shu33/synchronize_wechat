c96e3690483e9839d0d8a7a922c9ef216d2a0f01
zhaofeng-shu33 <616545598@qq.com>
2019-10-14 01:03:57 +0800

    save commit message in previous step

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 svn co -q "http://plugins.svn.wordpress.org/synchronize-wechat" /tmp/svn
 rsync -rc --exclude-from="./.distignore" ./ /tmp/svn/trunk --delete
+MESSAGE="$(git log -1 --pretty=%B)"
 cd /tmp/svn
 svn add . --force
-svn ci --no-auth-cache --non-interactive --username zhaofeng_shu33 --password $PASSWORD -m "$(git log -1 --pretty=%B)"
+svn ci --no-auth-cache --non-interactive --username zhaofeng_shu33 --password $PASSWORD -m "$MESSAGE"
